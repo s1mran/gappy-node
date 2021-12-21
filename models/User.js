913c7a4057f1bc5c5fc5ef5b1cb01f2db194b0bb
simran.sidhu <simran.sidhu@servicenow.com>
2021-12-21 20:44:22 +0530

    Deploy login and signup on heroku

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+// Create Schema
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        minlength: 1
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 1
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    createdAt: {
+        type: Date,
+        immutable: true,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    },
+});
+
+module.exports = User = mongoose.model("users", UserSchema);
